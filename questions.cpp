6bb2291399eeee873589c95d6c3378ce9326b58a
Abrar Fayaz <bhatabrar517@gmail.com>
2026-09-12 22:16:04 +0530

    Questions on cpp

---
+++ questions.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+class student{
+    public:
+    int rollno;
+    int marks;
+    string name;
+    void calculatepercentage(int m){
+        int total;
+    }
+};
+int main(){
+   student s1;
+   cout<<sizeof(s1);
+}
